d3c5e3ed9282c0b422602dc3af46337174af385e
mzikaadesta <m.zikaadesta@gmail.com>
2026-01-03 14:58:38 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,13 @@
-import streamlit as st
-
-st.write("Hello, *World!* :sunglasses:")
+st.markdown(
+    """
+    <a href="https://drive.google.com/drive/folders/ISI_LINK_DRIVE_KAMU" target="_blank">
+        <img src="assets/drive_button.png"
+             style="
+                width:180px;
+                border-radius:12px;
+                box-shadow:0 4px 10px rgba(0,0,0,0.2);
+             ">
+    </a>
+    """,
+    unsafe_allow_html=True
+)
